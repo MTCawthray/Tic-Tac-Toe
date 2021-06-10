4d34bda1559790b5688b846982a976b9d68c6ee8
Mark Cawthray <dr.cawthray@gmail.com>
2021-06-10 15:38:07 -0600

    Create query selectors for all of the game board buttons

---
+++ main.js
@@ -0,0 +1,10 @@
+// ------Variables----- //
+var topLeftBtn = document.querySelector('#a1');
+var topCenterBtn = document.querySelector('#a2');
+var topRightBtn = document.querySelector('#a3');
+var centerLeftBtn = document.querySelector('#b1');
+var centerBtn = document.querySelector('#b2');
+var centerRightBtn = document.querySelector('#b3');
+var bottomLeftBtn = document.querySelector('#c1');
+var bottomCenterBtn = document.querySelector('#c2');
+var bottomRightBtn = document.querySelector('#c3');
