55f153be85aa57b3901f4a7ed7142a69abaca33f
Mark Cawthray <dr.cawthray@gmail.com>
2021-06-10 15:42:38 -0600

    Create event listeners without event handlers for all of the buttons on the game board

--- main.js
+++ main.js
@@ -8,3 +8,16 @@ var centerRightBtn = document.querySelector('#b3');
 var bottomLeftBtn = document.querySelector('#c1');
 var bottomCenterBtn = document.querySelector('#c2');
 var bottomRightBtn = document.querySelector('#c3');
+
+
+
+//------ Event Listeners -------//
+topLeftBtn.addEventListener('click', '');
+topCenterBtn.addEventListener('click', '');
+topRightBtn.addEventListener('click', '');
+centerLeftBtn.addEventListener('click', '');
+centerBtn.addEventListener('click', '');
+centerRightBtn.addEventListener('click', '');
+bottomLeftBtn.addEventListener('click', '');
+bottomCenterBtn.addEventListener('click', '');
+bottomRightBtn.addEventListener('click', '');
