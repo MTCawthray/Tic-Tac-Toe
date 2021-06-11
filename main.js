07d09a6e188bcf3457ce9416a347f3022e36190c
Mark Cawthray <dr.cawthray@gmail.com>
2021-06-11 10:35:04 -0600

    Create clear game function and add new variables for current move and current player

--- main.js
+++ main.js
@@ -8,10 +8,17 @@ var centerRightBtn = document.querySelector('#b3');
 var bottomLeftBtn = document.querySelector('#c1');
 var bottomCenterBtn = document.querySelector('#c2');
 var bottomRightBtn = document.querySelector('#c3');
+var gameBoard = document.querySelector('.game-board');
+var currentPlayerDisplay = document.querySelector('.current-player');
 
+var winScenarios = [];
+var currentMove;
+var currentPlayer;
 
 
 //------ Event Listeners -------//
+window.addEventListener('load', function(event) {
+  clearGame(event)});
 topLeftBtn.addEventListener('click', '');
 topCenterBtn.addEventListener('click', '');
 topRightBtn.addEventListener('click', '');
@@ -21,3 +28,13 @@ centerRightBtn.addEventListener('click', '');
 bottomLeftBtn.addEventListener('click', '');
 bottomCenterBtn.addEventListener('click', '');
 bottomRightBtn.addEventListener('click', '');
+
+
+
+
+
+//-------- Functions --------//
+clearGame(event) {
+  event.preventDefault();
+  currentPlayerDisplay.innerText = 'New Game! X is up.';
+}
