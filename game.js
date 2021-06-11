3566dcb3e692ede7ecec7f737504dded3fb5ed1b
Mark Cawthray <dr.cawthray@gmail.com>
2021-06-11 14:07:39 -0600

    Add x positions and o positions as properties to the game class and set their values as arrays

--- game.js
+++ game.js
@@ -1,13 +1,14 @@
-// A Game should include:
-// Two Player instances
-// A way to keep track of the data for the game board
-// A way to keep track of which player’s turn it currently is
-// A way to check the Game’s board data for win conditions
-// A way to detect when a game is a draw (no one has won)
+
 class Game {
   constructor(player1, player2) {
     this.player1 = player1;
     this.player2 = player2;
+    this.xPositions = [];
+    this.oPositions = [];
+  }
+
+  switchPlayer() {
+
   }
 
   updateBoard() {
@@ -17,13 +18,14 @@ class Game {
 
   checkGame() {
     //This needs to check the current player's positions against the win scenarios Variable
-    //if it is a winning scenario, add 1 to player.wins, invoke a save to local storage for the win count, and invoke the clearGame method
+    for (var i = 0; i < winScenarios.length; i++) {
+      if (winScenarios[i].includes(/*player's positions*/))
+      //add 1 to player's wins in player instance
+      //invoke save to local saveToStorage with win count
+      //invoke clearGame method
+    }
     // if not a win, continue with game
     // if no more positions, decalare a draw
-    for (var i = 0; i < winScenarios.length; i++) {
-      if (winScenarios[i].includes())
-
-    }
   }
 
   clearGame() {
