d76f1e766791d2449382a0b31111a1dd996846a9
Mark Cawthray <dr.cawthray@gmail.com>
2021-06-13 22:35:05 -0600

    Create refresh game board helper function and invoke in clear game method

--- game.js
+++ game.js
@@ -55,22 +55,6 @@ class Game {
   clearGame() {
     makeNewGame();
     showNewGame();
-    gameBoard.innerHTML = `
-    <div class="a-row">
-      <button type="button" class="a1" id="a1" name="1"></button>
-      <button type="button" class="a2" id="a2" name="2"></button>
-      <button type="button" class="a3" id="a3" name="3"></button>
-    </div>
-    <div class="b-row">
-      <button type="button" class="b1" id="b1" name="4"></button>
-      <button type="button" class="b2" id="b2" name="5"></button>
-      <button type="button" class="b3" id="b3" name="6"></button>
-    </div>
-    <div class="c-row">
-      <button type="button" class="c1" id="c1" name="7"></button>
-      <button type="button" class="c2" id="c2" name="8"></button>
-      <button type="button" class="c3" id="c3" name="9"></button>
-    </div>
-    `;
+    refreshGameBoard();
   }
 }
--- main.js
+++ main.js
@@ -67,6 +67,26 @@ function showNewGame() {
   currentPlayerDisplay.innerText = 'New Game, Player 1 Is Up!';
 }
 
+function refreshGameBoard() {
+  gameBoard.innerHTML = `
+  <div class="a-row">
+    <button type="button" class="a1" id="a1" name="1"></button>
+    <button type="button" class="a2" id="a2" name="2"></button>
+    <button type="button" class="a3" id="a3" name="3"></button>
+  </div>
+  <div class="b-row">
+    <button type="button" class="b1" id="b1" name="4"></button>
+    <button type="button" class="b2" id="b2" name="5"></button>
+    <button type="button" class="b3" id="b3" name="6"></button>
+  </div>
+  <div class="c-row">
+    <button type="button" class="c1" id="c1" name="7"></button>
+    <button type="button" class="c2" id="c2" name="8"></button>
+    <button type="button" class="c3" id="c3" name="9"></button>
+  </div>
+  `;
+}
+
 function updateWinDisplay() {
   player1WinCount.innerText = `Wins: ${currentGame.player1.wins}`;
   player2WinCount.innerText = `Wins: ${currentGame.player2.wins}`;
